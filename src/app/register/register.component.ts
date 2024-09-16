9589848c30bbd3f9c9e8e3690b081ec1033d74f5
unknown <aneenasaneer17@gmail.com>
2024-09-16 13:42:19 +0400

    the added files are stored in to git

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,20 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements  OnInit {
 
-  userNAME=''
-  accNUM=''
-  psw=''
- 
+  
 
-  constructor(private ds:DataService, private router:Router)  { }
+  constructor(private ds:DataService, private router:Router, private fb:FormBuilder)  { }
+
+  //create reactive form of register form
+      registerForm=this.fb.group({
+      accNUM:['',[Validators.required,Validators.pattern('[0-9]+')]],
+      userNAME:['',[Validators.required,Validators.pattern('[A-Za-z]+')]],
+      psw:['',[Validators.required,Validators.pattern('[0-9A-Za-z]+')]]
+  })
+
+
+
+
   ngOnInit(): void {
     
   }
@@ -24,10 +33,11 @@ export class RegisterComponent implements  OnInit {
     
     
 
-    var username=this.userNAME
-    var accnumber=this.accNUM
-    var password=this.psw
+    var username=this.registerForm.value.userNAME
+    var accnumber=this.registerForm.value.accNUM
+    var password=this.registerForm.value.psw
 
+    if(this.registerForm.valid){
      const result=this.ds.register(username,accnumber,password)
       if(result){
         alert("registerd")
@@ -39,7 +49,10 @@ export class RegisterComponent implements  OnInit {
 
     console.log(username,accnumber,password);
   }
-  
+  else{
+    alert("invalid form")
+  }
+  }
   
 
 }
